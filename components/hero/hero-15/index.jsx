5919f4ef1ce83e5ceef0a58c8fd116815c8721ca
Gary Patel <pate0650@algonquinlive.com>
2023-03-25 16:04:12 -0400

    Hero image update

--- components/hero/hero-15/index.jsx
+++ components/hero/hero-15/index.jsx
@@ -5,7 +5,7 @@ const index = () => {
   return (
     <section
       className="banner-section -type-15"
-      style={{ backgroundImage: "url(images/index-15/header/bg.png)" }}
+      style={{ backgroundImage: "url(images/icons/bg-4.png)" }}
     >
       <div className="auto-container">
         <div className="cotnent-box">
