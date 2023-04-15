86cc4b975f8665a7c33b84720c22cb605993e5ca
healthcare-solutions <healthcareportal2@gmail.com>
2023-04-14 23:55:27 -0400

    Job list page search jobs

--- components/job-listing-pages/job-list/JobSearchForm.jsx
+++ components/job-listing-pages/job-list/JobSearchForm.jsx
@@ -1,8 +1,17 @@
+import { useDispatch, useSelector } from "react-redux";
 import Categories from "../components/Categories";
 import LocationBox from "../components/LocationBox";
 import SearchBox from "../components/SearchBox";
+import { setSearchFields } from "../../../features/search/searchSlice";
 
 const JobSearchForm = () => {
+  const dispatch = useDispatch()
+  const filterTerm = useSelector((state) => state.filter.jobList.keyword)
+  const filterLocation = useSelector((state) => state.filter.jobList.location)
+  const findJobs = () => {
+    dispatch(setSearchFields({ searchTerm: filterTerm, searchAddress: filterLocation }))
+    
+  }
   return (
     <div className="job-search-form">
       <div className="row">
@@ -24,7 +33,7 @@ const JobSearchForm = () => {
         {/* <!-- Form Group --> */}
 
         <div className="form-group col-lg-4 col-md-12 col-sm-12 text-right">
-          <button type="submit" className="theme-btn btn-style-one">
+          <button type="submit" className="theme-btn btn-style-one" onClick={findJobs}>
             Find Jobs
           </button>
         </div>
