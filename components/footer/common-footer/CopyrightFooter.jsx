8bd76b13864a58ac9368d428d4b9a3f8dd35e159
Mounish Soni <mounishsoni116@gmail.com>
2023-04-18 17:00:38 -0400

    fix scroll on small screen

--- components/footer/common-footer/CopyrightFooter.jsx
+++ components/footer/common-footer/CopyrightFooter.jsx
@@ -10,9 +10,26 @@ const CopyrightFooter = () => {
         © {new Date().getFullYear()} Immense Career. All Right Reserved.
       </p>
           </div>
-          <div className="social-links">
-            <Social />
+          <div className="contact-info">
+            {/* <span className="phone-num">
+              <span>Call us</span>
+              <a href="tel:1234567890">123 456 7890</a>
+            </span> */}
+            {/* <span className="address">
+              329 Queensberry Street, North Melbourne VIC <br />
+              3051, Australia.
+            </span> */}
+            <span style={{color: '#f5f5f5'}}>Email us:</span><br/>
+            <a
+              href="mailto:badrishsoni116@gmail.com"
+              className="email"
+              style={{ color: '#f5f5f5', fontFamily: 'console' }}>
+              <i>support@immensecareer.com</i>
+            </a>
           </div>
+          {/* <div className="social-links">
+            <Social />
+          </div> */}
         </div>
       </div>
     </div>
